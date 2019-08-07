d8d38bad18e099c05576747c31ada650a05a2cb9
27352 <solidity@gmx.com>
2019-08-07 11:43:21 -0400

    setting up different end point for Conviva touchstone

--- config/cbsallaccess-ps4-config.js
+++ config/cbsallaccess-ps4-config.js
@@ -28,8 +28,8 @@
             params: {
                 customerKey: "87a6b28bc7823e67a5bb2a0a6728c702afcae78d",
                 testCustomerKey: "ce4836fb66f6e081bcf6fea7df4531f22ac7ffbb",
-                prodServerUrl: "https://cbscom.testonly.conviva.com",
-                testServerUrl: "https://cbscom.testonly.conviva.com"
+                prodServerUrl: "cbscom-test.testonly.conviva.com",
+                testServerUrl: "cbscom-test.testonly.conviva.com"
             }
         }
     };
