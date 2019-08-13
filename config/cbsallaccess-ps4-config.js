02e10c0d7f42b869412e4a6ef4c490183765a899
27352 <solidity@gmx.com>
2019-08-13 13:18:00 -0400

    setting up for testing on ps4

--- config/cbsallaccess-ps4-config.js
+++ config/cbsallaccess-ps4-config.js
@@ -3,7 +3,7 @@
 
     w.vtg.config = {
         Adobe: {
-            enabled: true,
+            enabled: false,
             params: {
                 prodApiServer: "cbs-stage.hb-api.omtrdc.net",
                 devApiServer: "cbs-dev.hb-api.omtrdc.net",
@@ -15,7 +15,7 @@
             }
         },
         Nielsen: {
-            enabled: true,
+            enabled: false,
             params: {
                 prodApiServer: "cloudapi.imrworldwide.com/nmapi/v2",
                 devApiServer: "sandbox-cloudapi.imrworldwide.com/nmapi/v2",
@@ -24,7 +24,7 @@
             }
         },
         ConvivaPs4: {
-            enabled: true,
+            enabled: false,
             params: {
                 customerKey: "87a6b28bc7823e67a5bb2a0a6728c702afcae78d",
                 testCustomerKey: "ce4836fb66f6e081bcf6fea7df4531f22ac7ffbb",
