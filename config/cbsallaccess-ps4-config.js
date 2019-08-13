b776169f2139d914cfbe5b2e9c46419de62c8abe
27352 <43462051+27352@users.noreply.github.com>
2019-08-13 14:23:08 -0400

    config update

--- config/cbsallaccess-ps4-config.js
+++ config/cbsallaccess-ps4-config.js
@@ -3,7 +3,7 @@
 
     w.vtg.config = {
         Adobe: {
-            enabled: false,
+            enabled: true,
             params: {
                 prodApiServer: "cbs-stage.hb-api.omtrdc.net",
                 devApiServer: "cbs-dev.hb-api.omtrdc.net",
@@ -15,7 +15,7 @@
             }
         },
         Nielsen: {
-            enabled: false,
+            enabled: true,
             params: {
                 prodApiServer: "cloudapi.imrworldwide.com/nmapi/v2",
                 devApiServer: "sandbox-cloudapi.imrworldwide.com/nmapi/v2",
@@ -24,7 +24,7 @@
             }
         },
         ConvivaPs4: {
-            enabled: false,
+            enabled: true,
             params: {
                 customerKey: "87a6b28bc7823e67a5bb2a0a6728c702afcae78d",
                 testCustomerKey: "ce4836fb66f6e081bcf6fea7df4531f22ac7ffbb",
@@ -37,17 +37,15 @@
             params: {
                 //prodUrl: 'sparrow.cbsallaccess.ca',
                 //stageUrl: 'stage-sparrow.cbsallaccess.ca',
-                //interval: 3
+                interval: 10
             }
         },
         Comscore: {
             enabled: true,
             params: {
                 c2: "3002231",
-                c3: "partnerPlaceholder",
-                c4: "contentCreatorPlaceholder",
-                c6: "SeasonNumberEpisodeNumberPlaceholder",
-                partnerID: "cbs",
+                c3: "cbs-allaccess",
+                c4: "contentCreator",
                 publishersSecret: "2cb08ca4d095dd734a374dff8422c2e5"
             }
         }
