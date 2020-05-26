9d0469a38702d1535232cee883542a6932d6cf5f
roger.mayorga <solidity@gmx.com>
2020-05-26 13:59:31 -0400

    added globals to jest config

--- jest-sample/jest.config.js
+++ jest-sample/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    transform: { '^.+\\.ts?$': 'ts-jest' },
-    testEnvironment: 'node',
-    testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    "transform": { '^.+\\.ts?$': 'ts-jest' },
+    "testEnvironment": 'node',
+    "testRegex": '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
+    "moduleFileExtensions": ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    "globals": {
+        "ENGLISH_US": "en-us"
+    }
 };
